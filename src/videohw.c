151a086e9b794e947a3066999017fc33f1e993c4
Igor Zalatov <flyrouter@gmail.com>
2023-01-24 22:04:21 +0300

    Update videohw.c
    Experimental test to run Mini code inside OpenIPC on Hi3518EV200 processor.

--- src/videohw.c
+++ src/videohw.c
@@ -1245,7 +1245,7 @@ int stop_sdk() {
     return EXIT_SUCCESS;
 }
 
-#if HISILICON_SDK_GEN == 3
+#if ((HISILICON_SDK_GEN == 3) || (HISILICON_SDK_GEN == 2))
 #define BROKEN_MMAP
 #include "mmap.h"
 
